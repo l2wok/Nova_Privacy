2ecac4f5e1273d0fed403e026778a258c9aa460a
Andro <l22wok@gmail.com>
2016-07-26 17:57:11 +0300

    Update Routes.php
    remove Hooks, all work in Events

--- Modules/Privacy/Routes.php
+++ Modules/Privacy/Routes.php
@@ -1,8 +1,3 @@
 <?php
-
-use Helpers\Hooks;
-
-Hooks::addHook('footer', 'App\Modules\Privacy\Controllers\Privacy@getFooter');
-
 Router::get('privacy', 'App\Modules\Privacy\Controllers\Privacy@infoPage');
 Router::get('accept_cookies', 'App\Modules\Privacy\Controllers\Privacy@cookiesAccept');
